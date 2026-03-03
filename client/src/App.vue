28f04695b5e74bb41c5ce244409a733b6b109e8e
konstantinalen <ntinal039@gmail.com>
2026-03-03 09:37:05 +0000

    UI Design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <div class="bar">
+      <router-link to="/events" class="tabs">Track Books</router-link>
+      <router-link to="/books" class="tabs">All Books</router-link>
+    </div>
+  </nav>
+
+  <main class="content">
+    <router-view></router-view>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  nav
+  {
+    background-color: rgb(186, 189, 189);
+  }
+
+  .bar 
+  {
+    display: flex;
+    width: 100%;
+  }
+
+  .tabs 
+  {
+    margin-left: 33%;
+    cursor: pointer;
+    font-weight: bold;
+    font-size: larger;
+    padding: 15px;
+    text-decoration: none; 
+    color: #333; 
+  }
+
+  .tabs:hover 
+  {
+    color: rgb(80, 80, 80);
+  }
+
+  .router-link-active 
+  {
+    color: black;
+    border-bottom: 3px solid rgb(35, 35, 36);
+  }
+
+  .content 
+  {
+    padding: 20px;
+  }
 </style>
---
+++ client/src/routers.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Events from './components/Events.vue'
+import Books from './components/Books.vue'
+
+const routes = [
+  { path: '/', redirect: '/events' }, 
+  { path: '/events', component: Events },
+  { path: '/books', component: Books }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
